cb1438c5a6dbc0509b8b9a02ddd023bac2f2a0c9
Atakan Gulbahar <gulbahara17@itu.edu.tr>
2022-02-02 00:41:03 +0300

    Revert "Update FlutterLocalNotificationsPlugin.java"
    This reverts commit 6ae262631ba2b7a08ea0f359eb6d0af72c6c2447.

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
@@ -85,7 +85,7 @@ import io.flutter.view.FlutterMain;
  * FlutterLocalNotificationsPlugin
  */
 @Keep
-public class FlutterLocalNotificationsPlugin extends AppCompatActivity
+public class FlutterLocalNotificationsPlugin
         implements MethodCallHandler, PluginRegistry.NewIntentListener, FlutterPlugin, ActivityAware {
     private static final String SHARED_PREFERENCES_KEY = "notification_plugin_cache";
     private static final String DRAWABLE = "drawable";
